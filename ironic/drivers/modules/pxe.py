9fca2ffe00b4722c5233eeed82c20d4e09bc6ea8
Gonéri Le Bouder <goneri@redhat.com>
2016-02-23 16:01:41 -0500

    ipxe: add --timeout parameter to kernel and initrd
    In case of network issue during the download of the ramdisk or the
    kernel, ipxe can freeze. The default timeout value is "unlimited" [0] [1].
    If force_power_state_during_sync is False, ironic will just ignore the
    node until someone do an human intervention.
    
    By setting a timeout, we ensure ipxe will just give up. Depending on the
    BIOS configuration, a new boot attempt may be done immedialey without the
    hassle of a hard reboot.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1310778
    
    [0] http://ipxe.org/cmd/kernel
    [1] http://lists.ipxe.org/pipermail/ipxe-devel/2014-October/003829.html
    
    (cherry-picked from commit 404bfda9829ee8acf57fa0ec584b01f3118c3a5c)
    
    Closes-Bug: 1550417
    Change-Id: I472dfb73044df50849c9cf72de90e59151698376

--- ironic/drivers/modules/pxe.py
+++ ironic/drivers/modules/pxe.py
@@ -78,6 +78,10 @@ pxe_opts = [
                    'drivers/modules/boot.ipxe'),
                help=_('On ironic-conductor node, the path to the main iPXE '
                       'script file.')),
+    cfg.IntOpt('ipxe_timeout',
+               default=0,
+               help=_('Timeout value (in seconds) for downloading an image '
+                      'via iPXE. Defaults to 0 (no timeout)')),
 ]
 
 LOG = logging.getLogger(__name__)
@@ -294,7 +298,8 @@ def _build_pxe_config_options(task, pxe_info):
         'pxe_append_params': _get_pxe_conf_option(task, 'pxe_append_params'),
         'tftp_server': CONF.pxe.tftp_server,
         'aki_path': kernel,
-        'ari_path': ramdisk
+        'ari_path': ramdisk,
+        'ipxe_timeout': CONF.pxe.ipxe_timeout * 1000
     }
 
     return pxe_options
